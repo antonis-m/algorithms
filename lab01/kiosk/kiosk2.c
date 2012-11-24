24dbfb425eb4dcbf6efd666fba35040fc7c8af26
Antonis <antonismanousis@gmail.com>
2012-11-25 01:57:35 +0200

    bugs corrected

--- lab01/kiosk/kiosk2.c
+++ lab01/kiosk/kiosk2.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char **argv) {
 
 typedef struct {
-  long long int position;
+  long double position;
   long long int kiosks;
 } tuple ;
 
 FILE * fp;
 int i,flag_left,flag_right;
-long long int N, dist, var1,kiosks_to_move;
-long long int kiosks_to_move_left, kiosks_to_move_right, space_needed; //right stelnei aristera to left deksia.
-long double var2,shift_p,shift_neg,max_met_pos,max_met_neg,met_pos,met_neg;
+long double N, dist, var1,kiosks_to_move;
+long double kiosks_to_move_left, kiosks_to_move_right; //right stelnei aristera to left deksia.
+long double var2,shift_p,shift_neg,max_met_pos,max_met_neg,met_pos,met_neg,space_needed;
 tuple right,left;
 tuple * A;
  
  fp=fopen(argv[1],"r");
- fscanf(fp,"%lld", &N);        // find number of positions
- fscanf(fp,"%lld", &dist);    //  find min distance between kiosks.
+ fscanf(fp,"%Lf", &N);        // find number of positions
+ fscanf(fp,"%Lf", &dist);    //  find min distance between kiosks.
  
  A=malloc(sizeof(tuple)*N);
  flag_left=0;
  flag_right=0;                      //arxikopoiisi
  
  for (i=0; i<N; i++) {
-   fscanf(fp,"%lld",&A[i].position);
+   fscanf(fp,"%Lf",&A[i].position);
    fscanf(fp,"%lld",&A[i].kiosks);
  };
 
@@ -47,11 +46,11 @@ tuple * A;
  shift_p=shift_neg=0.0; 
  var2=0.0;
  for (i=1; i<N; i++) {
-   left.position +=var2;      //paratirw oti mono to left xreiazetai na kanw shift..
    met_pos = var2;           // check meta giati mono to aristero kineitai. ara to deksi den epireazetai. provlimatizomai metaksu shift_ var2
    kiosks_to_move=0;
    flag_left=flag_right;                                 //allazw ta flags
    left=right;                                           //egine gia na mi xasw tis allages pou ekana sto right pio prin
+   left.position +=var2;  //+=var2    //paratirw oti mono to left xreiazetai na kanw shift..
    right=A[i];
    
    if (right.kiosks%2 == 0)   
@@ -63,7 +62,7 @@ tuple * A;
    if ((flag_left==1)&&(flag_right==1)) {       
      
      right.kiosks=right.kiosks/2;
-     var1=right.position-left.position;                   //existing distance 
+     var1=abs(right.position-left.position);                   //existing distance 
      kiosks_to_move = left.kiosks + right.kiosks;        //  posa kioskia 8a metakinisw ap tis arxikes tous 8eseis.. 
      kiosks_to_move_right  = left.kiosks;
      kiosks_to_move_left   = right.kiosks;  
@@ -93,7 +92,7 @@ tuple * A;
    } else if ((flag_left==0)&&(flag_right==0)) {
      
      right.kiosks= (right.kiosks/2) +1;
-     var1=right.position-left.position;
+     var1=abs(right.position-left.position);
      kiosks_to_move=left.kiosks-1 + right.kiosks-1;    
      kiosks_to_move_right= left.kiosks-1;
      kiosks_to_move_left = right.kiosks-1;
@@ -123,7 +122,7 @@ tuple * A;
    } else if ((flag_left==1)&&(flag_right==0)) {
      
      right.kiosks=(right.kiosks/2)+1;
-     var1=right.position-left.position;
+     var1=abs(right.position-left.position);
      kiosks_to_move= left.kiosks + right.kiosks-1;
      kiosks_to_move_right= left.kiosks;
      kiosks_to_move_left=  right.kiosks-1;
@@ -153,7 +152,7 @@ tuple * A;
    } else if ((flag_left==0)&&(flag_right==1)) {
      
      right.kiosks=right.kiosks/2;
-     var1=right.position - left.position;
+     var1=abs(right.position - left.position);
      kiosks_to_move= left.kiosks-1 + right.kiosks;
      kiosks_to_move_right =left.kiosks-1; 
      kiosks_to_move_left= right.kiosks;
@@ -168,9 +167,10 @@ tuple * A;
      } else { 
 
        var2=(space_needed -var1)/2.0;                    // isotimo anoigma deksia kai aristera.
+       printf("%Lf %Lf \n",var2,space_needed);
        shift_p += var2;
        shift_neg += -var2;
-       max_met_pos +=var2;
+       max_met_pos +=var2 ; // edw exw kapoia aporia.
        max_met_neg -=var2;              
        met_pos=-var2+(left.kiosks-1)*dist; 
        if (met_pos>max_met_pos)
@@ -184,6 +184,19 @@ tuple * A;
    
  }
      
+
+left=right;          // prepei na upologistei i metakinisi gia to teleftaio stoixeio.
+left.position=var2;  // na 8umithw meta sto debugging na allaksw me shift pos
+met_pos=var2;
+flag_left=flag_right;
+if (flag_left==1) {
+met_pos+= (left.kiosks-1)*dist + dist/2.0;
+} else {
+met_pos +=(left.kiosks)*dist;
+};
+if (met_pos>max_met_pos)
+  max_met_pos=met_pos; 
+
 
 printf("%Lf %Lf \n", max_met_pos,max_met_neg);
 free(A);
