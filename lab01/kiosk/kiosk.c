fd2151e86e8c32e82bbe1e69ed0c054e970ac610
Antonis <antonismanousis@gmail.com>
2012-12-01 00:36:39 +0200

    finally done

--- lab01/kiosk/kiosk.c
+++ lab01/kiosk/kiosk.c
@@ -10,13 +10,13 @@ long double backtrack,offset, offset_left, offset_right, res, right,left,p_right
 long long int * A;
 
 
-fp=fopen(argv[1],"r");
+//fp=fopen(argv[1],"r");
 
-fscanf(fp, "%lld", &N);
-fscanf(fp, "%lld", &dist);
+scanf( "%lld", &N);
+scanf( "%lld", &dist);
 
-fscanf(fp, "%lld", &pos);
-fscanf(fp, "%lld", &kiosks);
+scanf( "%lld", &pos);
+scanf( "%lld", &kiosks);
 
 
  offset = (kiosks-1)*dist;
@@ -26,30 +26,31 @@ fscanf(fp, "%lld", &kiosks);
  backtrack=0;
 for (i=1; i<N; i++) {
 
- fscanf(fp, "%lld", &pos);
- fscanf(fp, "%lld", &kiosks);
+ scanf( "%lld", &pos);
+ scanf( "%lld", &kiosks);
 
  p_right=right;
 
 
  offset = (kiosks-1)*dist;
  offset_left=offset_right=offset/2.0;
+// printf("%ld %lld %Lf \n",i,kiosks,offset_left);
  left=pos-offset_left;
  right=pos+offset_right;
  mmove=p_right+dist-left;
-
- if (mmove>0) {
+ 
+ if (mmove>=0) {
     if (mmove>fabs(res-offset_left)) {
       interm= mmove- fabs(offset_left-res);
       
-      //printf("%Lf %Lf %Lf %Lf\n",interm/2.0, mmove, offset_left, res);
+  //    printf("%Lf %Lf %Lf %Lf\n",interm/2.0, mmove, offset_left, res);
       if(offset_left>res)
         res = interm/2.0 + offset_left;
       else
         res = interm/2.0 +res;  
       right=right+res-offset_left;
       //backtrack+= (res-offset_left)/2.0;
-      //printf("%ld, %Lf 1\n",i,res);
+    //  printf("%ld, %Lf 1\n",i,res);
    }  else {
         if(res>offset_left) 
           res=res;
@@ -57,7 +58,7 @@ for (i=1; i<N; i++) {
           res= offset_left;
         right+=mmove;
         //backtrack+=mmove/2.0;
-        //printf("%ld, %Lf 2\n",i,res);
+      //  printf("%ld, %Lf 2\n",i,res);
     } 
   
   } else if (mmove<0) {
@@ -68,7 +69,7 @@ for (i=1; i<N; i++) {
       if (mmove<interm) 
        right-=mmove; 
       else right-= interm;
-      
+     // printf("%ld, %Lf 3\n", i,res);
   }
  
 };
