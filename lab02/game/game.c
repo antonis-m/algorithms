45c367ac5eb7f91dfd5789b307a4b8a8dba9af9e
Antonis <antonismanousis@gmail.com>
2013-01-01 20:32:47 +0200

    defined array structure

--- lab02/game/game.c
+++ lab02/game/game.c
@@ -11,17 +11,23 @@ long long int * array;
 long long int **list;
 scanf( "%lld", &N);
 array=malloc(sizeof(long long int)*N);
-*list=malloc(sizeof(long long int *)*N);
+list=(long long int **)malloc(sizeof(long long int *)*N);
 
 for (i=0; i<N; i++){
- list[i]=malloc(sizeof(long long int)*(N-i));
+ list[i]=(long long int *)malloc(sizeof(long long int)*(N-i));
 }
 
+
+for (i=0; i<N; i++) {
+ list[N-1-i][i]=0; 
+}
 
 for (i=0; i<N; i++){
    scanf("%lld", &array[i]);
 }
 
-
+for (i=0; i<N; i++)
+ free(list[i]);
+free(list);
 return 0;
 }
