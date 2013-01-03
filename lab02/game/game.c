c626b834020c305819619377b576aa48a81542db
Antonis <antonismanousis@gmail.com>
2013-01-03 02:53:22 +0200

    finished with list and all the indexes.

--- lab02/game/game.c
+++ lab02/game/game.c
@@ -8,24 +8,22 @@
 int main() {
 long long int N,i,j,l,k,mik,mkj,money,temp;
 long long int * array, * cum_sum;
-long long int **list,** list2;
+long long int **list;
 scanf( "%lld", &N);
 
 array=malloc(sizeof(long long int)*N);
 cum_sum=malloc(sizeof(long long int)*N);
 
 list=(long long int **)malloc(sizeof(long long int *)*N);
-//list2=(long long int **)malloc(sizeof(long long int *)*N);
 
-for (i=0; i<N; i++){
+for (i=0; i<N; i++)
  list[i]=(long long int *)malloc(sizeof(long long int)*(N-i));
-// list2[i]=(long long int *)malloc(sizeof(long long int )*(N-i));
-}
+
 
 money=0;
-for (i=0; i<N; i++) {
- list[N-i-1][i]=0;  // na koitaksw to -1 
-}
+for (i=0; i<N; i++) 
+ list[N-i-1][i]=0;  
+
 for (i=0; i<N; i++) 
    scanf("%lld", &array[i]);
 
@@ -36,10 +34,9 @@ for(i=1; i<N; i++)
 for (l=2; l<=N; l++ ) {   //to mikos tis akolouthias pou koitaw
    for(i=0; i<=(N-l); i++) {
       j=i+l-1;
-      list[N-1-j][i]=-1;
-      printf("%lld %lld\n",N-1-j,i);
+      list[N-j-1][i]=-1;
+//      printf("%lld %lld %lld %lld\n",N/*-1*/-j,j,i,N-i-1);
       for (k=i;k<=j;k++) {
-//          printf("%lld %lld %lld\n",i,j,k);
           if (k==0) {
             mik = 0;
             mkj = cum_sum[j]-cum_sum[0]+array[0];
@@ -47,29 +44,25 @@ for (l=2; l<=N; l++ ) {   //to mikos tis akolouthias pou koitaw
           mik = cum_sum[k-1]-cum_sum[i]+array[i]; 
           mkj = cum_sum[j]-cum_sum[k-1];
           }
-//          printf("%lld %lld, mik mkj\n",mik,mkj);
           temp=min(mik,mkj);
-          if (temp > list[N-1-j][i])
-          list[N-1-j][i]=temp;
-          
+          if (temp > list[N-j-1][i]) {
+          list[N-j-1][i]=temp;
+          }
       } 
-//printf("%lld %lld %lld \n",i,N-1-j,list[N-1-j][i]);
+//printf("%lld %lld %lld \n",N-j-1,i,list[N-j-1][i]);
    }
 //money += list[N-1-j][i];
-//printf("%lld %lld %lld \n",j,i,list[N-1-j][i]);
 }
 
 printf("%lld\n",money);
 
 
-for (i=0; i<N; i++){
+for (i=0; i<N; i++)
  free(list[i]);
-// free(list2[i]);
-}
+
 
 free(list);
 free(array);
 free(cum_sum);
-//free(list2);
 return 0;
 }
