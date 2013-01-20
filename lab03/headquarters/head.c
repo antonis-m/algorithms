258a0207d16e0022cb00dbf34eab2795f35789b3
Antonis <antonismanousis@gmail.com>
2013-01-20 22:51:35 +0200

    working on fastpower

--- lab03/headquarters/head.c
+++ lab03/headquarters/head.c
@@ -2,16 +2,34 @@
 #include <stdlib.h>
 
 
+void fastpower (int **,long long int ,long long int);
 int main () {
 
-long long int k,N,s,t,M,i;
+long long int k,N,s,t,M,i,temp1,temp2;
 int **array;
 
 scanf("%lld %lld %lld %lld %lld", &k, &N, &M, &s, &t);
 array=(int**)malloc(N*sizeof(int *));
-for(i=0; i<N-1; i++)
+for(i=0; i<N; i++)
   array[i]=(int *)calloc(N,sizeof(int));
 
+for (i=0; i<M; i++) { 
+  scanf("%lld %lld", &temp1, &temp2);
+  array[temp1][temp2]=1;
+}
 
 return 0;
+}
+
+
+void fastpower(array, k,N) {
+ long long int
+ int **temp;
+ temp=(int**)malloc(N*sizeof(int *));
+ for(i=0; i<N; i++)
+   array[i]=(int *)calloc(N,sizeof(int));
+ for(i=0; i<N; i++) 
+   array[i][i]=1;
+
+
 }
