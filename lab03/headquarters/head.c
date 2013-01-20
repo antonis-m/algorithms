14c72f9d5243ada0c73b58f1c4c50f01a0ed4949
Antonis <antonismanousis@gmail.com>
2013-01-21 00:45:52 +0200

    time to start debugging

--- lab03/headquarters/head.c
+++ lab03/headquarters/head.c
@@ -5,7 +5,7 @@
 void fastpower (int **,long long int k,long long int N);
 int main () {
 
-long long int k,N,s,t,M,i,temp1,temp2;
+long long int k,N,s,t,M,i,j,temp1,temp2;
 int **array;
 
 scanf("%lld %lld %lld %lld %lld", &k, &N, &M, &s, &t);
@@ -19,27 +19,60 @@ for (i=0; i<M; i++) {
 }
 
 fastpower(array,k,N);
+for (i=0; i<N; i++){ 
+ for (j=0; j<N; j++)
+    printf("%d ",array[i][j]);
+ printf("\n"); 
+}
 return 0;
 }
 
 
 void fastpower(int **array, long long int k,long long int N) {
- long long int i; 
- int **temp;
+ long long int i,j,l; 
+ int **temp, **C;
+ int sum;
  temp=(int**)malloc(N*sizeof(int *));
- for(i=0; i<N; i++)
+ C= (int**)malloc(N*sizeof(int *));
+
+ for(i=0; i<N; i++) {
    temp[i]=(int *)calloc(N,sizeof(int));
+   C[i]=(int *)calloc(N,sizeof(int));
+  }
  for(i=0; i<N; i++) 
    temp[i][i]=1;
+
  while (k>0) {
- if ((k&1) == 0)
-  ; // temp=temp*array;
- else { 
-    k= k/2;
-   // array=array*array;
+ if ((k&1) == 0) { 
+   for (i=0; i<N; i++) {
+     for (j=0; j<N; j++) {
+       sum=0;
+       for (l=0; l<N; l++)
+         sum += temp[i][l]*array[l][j];  
+         C[i][j]=sum;
+     }
+   }
+ **temp=**C;  
+ k--;
+ }
+
+  k= k/2;
+  for (i=0; i<N; i++) {
+    for (j=0; j<N; j++) {
+      sum=0;
+      for (l=0; l<N; l++)
+        sum += array[i][l]*array[l][j];  
+        C[i][j]=sum;
+    }
   }
+ **array=**C;  
  }
 array=temp;
+for (i=0; i<N; i++) {
+  free(temp[i]);
+  free(C[i]);
+}
+free (temp); free(C);
 }
 
 
