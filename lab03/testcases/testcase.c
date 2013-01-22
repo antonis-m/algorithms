bfe5253291ed1721bc3d72b35e88b075fa60dd5f
Antonis <antonismanousis@gmail.com>
2013-01-22 23:13:52 +0200

    8a paiksw me aplo pinaka gonewn

--- lab03/testcases/testcase.c
+++ lab03/testcases/testcase.c
@@ -2,11 +2,20 @@
 #include <stdlib.h>
 
 int main () {
-long long int nodes;
-scanf("%lld", &nodes);
+long long int N,i,u,v,w;
+long long int ** array;
+scanf("%lld", &N);
+
+array=(long long int **)malloc(N*sizeof(long long int));
+for(i=0; i<N; i++)
+  array[i]=(long long int *)malloc(3*sizeof(long long int));
 
 
 
+
+for (i=0; i<N; i++)
+  free(array[i]);
+free(array);
 
 return 0; 
 }
