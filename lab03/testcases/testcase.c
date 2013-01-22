ae32c4d61e044d8ac8359fed1f266deb72aa8486
Antonis <antonismanousis@gmail.com>
2013-01-22 23:48:04 +0200

    finished loading

--- lab03/testcases/testcase.c
+++ lab03/testcases/testcase.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 
 int main () {
-long long int N,i,u,v,w;
+long long int N,i,weight;
 long long int ** array;
 scanf("%lld", &N);
 
 array=(long long int **)malloc(N*sizeof(long long int));
 for(i=0; i<N; i++)
   array[i]=(long long int *)malloc(3*sizeof(long long int));
+weight=0;
 
+for (i=0; i<N; i++) {
+ scanf("%lld",&array[i][0]);
+ scanf("%lld",&array[i][1]);
+ scanf("%lld",&array[i][2]);
+ weight += array[i][2];
+
+}
 
 
 
